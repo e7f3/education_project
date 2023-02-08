b8c0f322f46fd4feded27edfe1eea1d55be291b4
Ilia <ilya@leadsforfinance.com>
2023-02-08 12:31:55 +0400

    ci: test new i18next config for ci

--- src/shared/config/i18n/i18nForStorybook.ts
+++ src/shared/config/i18n/i18nForStorybook.ts
@@ -7,6 +7,7 @@ import { initReactI18next } from 'react-i18next'
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
+  .use(Backend)
   .use(
     resourcesToBackend(
       (lng: string, ns: string) =>
