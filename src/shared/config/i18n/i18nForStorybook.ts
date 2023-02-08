441ddcc318d0bf0ab051524c14581248a8a02b57
Ilia <ilya@leadsforfinance.com>
2023-02-08 12:27:25 +0400

    ci: test new i18next config for ci

--- src/shared/config/i18n/i18nForStorybook.ts
+++ src/shared/config/i18n/i18nForStorybook.ts
@@ -4,23 +4,9 @@ import Backend from 'i18next-http-backend'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next'
 
-// const ns = ['common']
-// const supportedLngs = ['ru', 'en']
-// const resources = ns.reduce((acc, n) => {
-//   supportedLngs.forEach((lng) => {
-//     if (!acc[lng]) acc[lng] = {}
-//     acc[lng] = {
-//       ...acc[lng],
-//       [n]: require(`../public/locales/${lng}/${n}.json`),
-//     }
-//   })
-//   return acc
-// }, {})
-
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
-  .use(Backend)
   .use(
     resourcesToBackend(
       (lng: string, ns: string) =>
@@ -30,16 +16,16 @@ i18n
   .init({
     fallbackLng: 'ru',
     lng: 'ru',
-    debug: false,
+    debug: true, // change to true on push
     react: {
       useSuspense: false,
     },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
+    // backend: {
+    //   loadPath: '/locales/{{lng}}/{{ns}}.json',
+    // },
   })
 
 export default i18n
--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,19 +1,27 @@
 import { fireEvent, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 
-import {
-  renderWithTranslation,
-} from 'shared/lib/testHelpers/renderWithTranslation/renderWithTranslation'
+import { renderWithTranslation } 
+  from 'shared/lib/testHelpers/renderWithTranslation/renderWithTranslation'
 import { Sidebar } from 'widgets/Sidebar/index'
 
 describe('Sidebar.test', () => {
   test('Simple Sidebar.test', () => {
-    renderWithTranslation(<Sidebar />)
+    renderWithTranslation(
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>
+    )
 
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('Collapse Sidebar.test', () => {
-    renderWithTranslation(<Sidebar />)
+    renderWithTranslation(
+      <BrowserRouter>
+        <Sidebar />
+      </BrowserRouter>
+    )
 
     const collapseButton = screen.getByTestId('sidebar-collapse-button')
 
